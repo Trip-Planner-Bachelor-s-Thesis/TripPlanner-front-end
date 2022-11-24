689888b7868e3deef3d7354d3d0302d53adf806d
marcinswierkot <marc.swierkot@gmail.com>
2022-11-25 00:25:21 +0100

    Refine Navigation bar

--- src/components/Layout/MainContent.js
+++ src/components/Layout/MainContent.js
@@ -1,12 +1,12 @@
 import { Fragment } from 'react';
 
 import styles from "./MainContent.module.css"
-import NavigationBar from './NavigationBar';
+import NavigationBarBootstrap from './NavigationBarBootstrap';
 
 const MainContent = (props) => {
   return (
     <Fragment>
-      <NavigationBar />
+      <NavigationBarBootstrap />
       <main className={styles.main}>{props.children}</main>
     </Fragment>
   );
---
+++ src/components/Layout/NavigationBarBootstrap.js
@@ -0,0 +1,63 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Avatar from "@mui/joy/Avatar";
+import Button from "react-bootstrap/Button";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+
+import LogRegisterContext from "../../contexts/log-register-context";
+
+const NavigationBarBootstrap = () => {
+  const logRegisterContext = useContext(LogRegisterContext);
+
+  const logoutHandler = () => {
+    logRegisterContext.logout();
+  };
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid style={{ margin: "0 10rem" }}>
+        <Navbar.Brand as={NavLink} to="/">
+          Trip Planner
+        </Navbar.Brand>
+        <Nav className="d-flex justify-content-end">
+          {!logRegisterContext.token && (
+            <Nav.Link style={{ margin: "0 0.5rem" }} as={NavLink} to="/auth">
+              Login
+            </Nav.Link>
+          )}
+          {logRegisterContext.token && (
+            <Nav.Link style={{ margin: "0 0.5rem" }} as={NavLink} to="/trips">
+              All trips
+            </Nav.Link>
+          )}
+          {logRegisterContext.token && (
+            <Nav.Link style={{ margin: "0 0.5rem" }} as={NavLink} to="/my-trips">
+              My trips
+            </Nav.Link>
+          )}
+          {logRegisterContext.token && (
+            <Nav.Link style={{ margin: "0 0.5rem" }} as={NavLink} to="/new-trip">
+              Create trip
+            </Nav.Link>
+          )}
+          {logRegisterContext.token && <Avatar style={{ marginLeft: "0.5rem" }}>JG</Avatar>}
+          {logRegisterContext.token && (
+            <NavDropdown>
+              <NavDropdown.Item as={NavLink} to="/profile">
+                Profile
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Button} onClick={logoutHandler}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavigationBarBootstrap;
